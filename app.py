5df537d68c4a289e552ad380a8a2f97268cdfb10
nixxxxe <denissseavila21@gmail.com>
2024-09-03 00:28:11 +0800

    toml

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
+from st_pages import add_page_title, get_nav_from_toml
 
-st.write('Hello World!')
+st.set_page_config(layout="wide")
+
+# Load the navigation from the pages_sections.toml file
+nav = get_nav_from_toml(".streamlit/pages_sections.toml")
+
+# Add a logo if you have one
+#st.image("logo.png", width=100)
+
+pg = st.navigation(nav)
+
+add_page_title(pg)
+
+pg.run()
